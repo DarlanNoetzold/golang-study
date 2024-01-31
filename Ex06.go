593401d069693e809c8e38842ee0ec04819dc7a9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-31 12:56:08 -0300

    Create Ex06.go

---
+++ Ex06.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x bool
+
+func main() {
+	fmt.Println(x) // zero value
+	x = true
+	fmt.Println(x) // valor atribuido
+	x = 10 == 100
+	y := 10 == 100
+	z := 10 > 100
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
