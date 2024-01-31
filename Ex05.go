3faef9af5ac411e2002784b88b198a72be5f6b7c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-31 12:56:06 -0300

    Create Ex05.go

---
+++ Ex05.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+type hotdog int
+
+var x hotdog
+
+var y int
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+	fmt.Println("↑ foi x.\n↓ é y!")
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+
+}
