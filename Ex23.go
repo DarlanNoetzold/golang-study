0a93134a8990a287ea0b88d372a285333969c2ba
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-02-17 08:56:23 -0300

    Create Ex23.go

---
+++ Ex23.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	array := [5]int{10, 20, 30, 40, 50}
+	
+	for i, v := range array {
+		fmt.Println(i, v)
+	}
+
+	fmt.Printf("%T", array)
+}
+
